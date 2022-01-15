df3c57fca9b38c462ee0707c8afd9230baf690d2
João Palmeiro <joaommpalmeiro@gmail.com>
2022-01-15 18:38:08 +0000

    Update bar chart

--- src/BarChart.jsx
+++ src/BarChart.jsx
@@ -2,21 +2,32 @@ import { Group } from '@visx/group';
 import { scaleLinear } from 'd3-scale';
 
 import data from './tools_counts.json';
+import { combineChartDimensions, getRandomColor } from './utils';
+
+const defaultBarHeight = 50; // px
 
 // Accessors
-const xAccessor = (d) => d.use_count;
+const xAccessor = (d) => d.use_count / d.total_count;
 const yAccessor = (d) => d.tool;
-const totalAccessor = (d) => d.total_count;
 
 function BarChart() {
-    const xScale = scaleLinear().domain([0, 1]).range([0, 300]).nice();
+    const height = data.length * defaultBarHeight;
+    const dimensions = combineChartDimensions({ width: 300, height });
+    // console.log(dimensions);
+
+    const xScale = scaleLinear().domain([0, 1]).range([0, dimensions.boundedWidth]).nice();
     const xAccessorScaled = (d) => xScale(xAccessor(d));
 
     return (
         <svg>
             <Group>
                 {data.map((d) => (
-                    <rect key={yAccessor(d)} />
+                    <rect
+                        key={yAccessor(d)}
+                        width={xAccessorScaled(d)}
+                        height={defaultBarHeight}
+                        fill={getRandomColor()}
+                    />
                 ))}
             </Group>
         </svg>
--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,13 @@
 // Source: Fullstack D3 and Data Visualization by Amelia Wattenberger (https://wattenberger.com/)
 export const combineChartDimensions = (dimensions) => {
+    // Amelia's default values.
+    // const parsedDimensions = {
+    //     marginTop: 40,
+    //     marginRight: 30,
+    //     marginBottom: 40,
+    //     marginLeft: 75,
+    //     ...dimensions
+    // };
     const parsedDimensions = {
         marginTop: 0,
         marginRight: 0,
@@ -20,3 +28,8 @@ export const combineChartDimensions = (dimensions) => {
         )
     };
 };
+
+// Source:
+// - https://www.paulirish.com/2009/random-hex-color-code-snippets/ by Paul Irish
+// - https://syntax.fm/show/419/js-one-liners
+export const getRandomColor = () => '#' + Math.floor(Math.random() * 16777215).toString(16);
