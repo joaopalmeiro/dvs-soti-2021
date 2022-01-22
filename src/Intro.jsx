c724763801e29093c01b8f735c5fc1918fad7a35
João Palmeiro <joaommpalmeiro@gmail.com>
2022-01-22 20:24:09 +0000

    Update Intro.jsx

--- src/Intro.jsx
+++ src/Intro.jsx
@@ -6,7 +6,13 @@ function Intro() {
     return (
         <article>
             <Heading>the Survey</Heading>
-            <Text>The 2021 Data Visualization State of the Industry Survey results are out!</Text>
+            <Text size="lg">
+                The{' '}
+                <Text component="span" weight="bold" size="lg">
+                    2021 Data Visualization State of the Industry Survey
+                </Text>{' '}
+                results are out!
+            </Text>
         </article>
     );
 }
